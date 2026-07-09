b16372696d354ce5950a67e4d368b09a42eb0a69
ashishkumarjaiswal999 <ashish936631@gmail.com>
2026-07-09 15:41:14 +0530

    10

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ console.log('start');
 async function main() {
     let a = await fetch("http://127.0.0.1:5500/assets/songs/");
     let b= await a.text();
-    console.log(b);
+    let c=document.createElement("div");
+    c.innerHTML=b;
+    let e=c.getElementsByTagName("a");
+    console.log(e); 
 }
 main();
+
+
+
