921ef795a4fd7fd351d56f8eeb37d78ec1cbc26b
ashishkumarjaiswal999 <ashish936631@gmail.com>
2026-07-10 13:04:53 +0530

    14

--- script.js
+++ script.js
@@ -1,57 +1,41 @@
-// async function getsongs() {
-//     let a = await fetch("http://127.0.0.1:5500/assets/songs/");
-//     let b = await a.text();
-//     let c = document.createElement("div");
-//     c.innerHTML = b;
-//     let e = c.getElementsByTagName("a");
-//     let song = [];
-//     for (let index = 0; index < e.length; index++) {
-//         const element = e[index];
-//         if (element.href.endsWith(".mp3")) {
-//             song.push(element.href);
-//         }
-//     }
-//     return song;
-// }
-
-
-
-// async function main() {
-//     let songs = await getsongs();
-//     console.log(songs);
-//     let audio = new Audio(songs[]);
-//     audio.play();
-//     audio.addEventListener("loadedmetadata", () => {
-//         console.log(audio.duration);
-//     })
-// }    
-// main();
-
-
-
-
-
-console.log("hi")
 async function getsongs() {
-    let a = await fetch("/assets/songs");
+    let a = await fetch("http://127.0.0.1:5500/assets/songs/");
     let b = await a.text();
     let c = document.createElement("div");
     c.innerHTML = b;
-    let d = c.getElementsByTagName("a");
-    let s = [];
-    for (let index = 0; index < d.length; index++) {
-        const element = d[index];
+    let e = c.getElementsByTagName("a");
+    let song = [];
+    for (let index = 0; index < e.length; index++) {
+        const element = e[index];
         if (element.href.endsWith(".mp3")) {
-            s.push(element.href);
+            song.push(element.href);
         }
     }
-    return s;
+    return song;
 }
+
+
+
 async function main() {
-    let z=await getsongs();
-    console.log(z);
-    let song=new Audio(z[1]);
-    song.play()
+    let songs = await getsongs();
+    let audio = new Audio(songs[1]);
+
+    audio.addEventListener("loadedmetadata", () => {
+        console.log(audio.duration, audio.textTracks, audio.currentTime);
+    })
+    let byn = document.querySelector(".play");
+    byn.addEventListener("click", () => {
+        //To play and pause using play btn
+        if (audio.paused) {
+            audio.play()
+        } else {
+            audio.pause();
+        }
+        //currentTime
+        console.log(audio.currentTime);
+    })
     
 }
 main();
+
+
