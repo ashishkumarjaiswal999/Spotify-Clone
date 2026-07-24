ef05fb68b3778fd76d387ef6753928e57ab977f4
ashishkumarjaiswal999 <ashish936631@gmail.com>
2026-07-24 19:12:12 +0530

    finalized

--- script.js
+++ script.js
@@ -1,118 +1,93 @@
-// async function fetcher() {
-//     let fetch1 = await fetch("/assets/Songs");
-//     let fetch2 = await fetch1.text();
-//     let fetch3 = document.createElement("div");
-//     fetch3.innerHTML = fetch2;
-//     let fetch4 = fetch3.getElementsByTagName("a")
-//     let fetch5 = [];
-//     for (let index = 0; index < fetch4.length; index++) {
-//         if (fetch4[index].href.endsWith(".mp3")) {
-//             fetch5.push(fetch4[index].href)
-//         }
-
-//     }
-//     return fetch5;
-// }
-// async function main() {
-//     let songarray = await fetcher();
-//     let audio = new Audio(songarray[0]);
-//     let jplay = document.querySelector(".play");
-//     let jdur = document.querySelector(".dur");
-//     let jlist = document.querySelector(".oip");
-//     let jsongname = document.querySelector(".songname");
-//     let jeachsong = document.querySelector(".oip").getElementsByTagName("li")
-
-//     jplay.addEventListener("click", () => {
-//         if (audio.paused) {
-//             audio.play();
-//             jplay.src = "/assets/pause.svg";
-//         } else {
-//             audio.pause();
-//             jplay.src = "/assets/play.svg";
-//         }
-//     })
-
-
-//     audio.addEventListener("loadedmetadata", () => {
-//         let orgmins = Math.floor(audio.duration / 60);
-//         let orgsecs = Math.floor(audio.duration % 60);
-//         jdur.innerHTML = `0:00/${orgmins}:${orgsecs}`;
-//     })
-//     let songname1 = songarray[0];
-//     let songname2 = songname1.split("/Songs/")[1];
-//     let songname3 = decodeURIComponent(songname2)
-//     songname4 = songname3.replaceAll(".mp3", "");
-//     jsongname.innerHTML = songname4;
-
-
-//     audio.addEventListener("timeupdate", () => {
-//         let mins = Math.floor(audio.currentTime / 60);
-//         let secs = Math.floor(audio.currentTime % 60);
-//         let orgmins = Math.floor(audio.duration / 60);
-//         let orgsecs = Math.floor(audio.duration % 60);
-//         if (secs < 10) {
-//             secs = "0" + secs;
-//         }
-//         jdur.innerHTML = `${mins}:${secs}/${orgmins}:${orgsecs}`;
-//     })
-
-//     for (let index = 0; index < songarray.length; index++) {
-//         let songname1 = songarray[index];
-//         let songname2 = songname1.split("/Songs/")[1];
-//         let songname3 = decodeURIComponent(songname2)
-//         songname4 = songname3.replaceAll(".mp3", "");
-//         jlist.innerHTML += `<li class="normalhov music">${songname4}</li>`;
-//     }
-//     console.log(jeachsong)
-//     for (let index = 0; index < songarray.length; index++) {
-//         jeachsong[index].addEventListener("click", () => {
-//             audio.src = songarray[index];
-//             if (audio.paused) {
-//                 audio.play();
-//                 jplay.src = "/assets/pause.svg";
-//             } else {
-//                 audio.pause();
-//                 jplay.src = "/assets/play.svg";
-//             }
-//             let songname1 = songarray[index];
-//             let songname2 = songname1.split("/Songs/")[1];
-//             let songname3 = decodeURIComponent(songname2)
-//             songname4 = songname3.replaceAll(".mp3", "");
-//             jsongname.innerHTML = songname4;
-//         })
-
-//     }
-//     document.addEventListener("keydown", (e) => {
-//         if (e.key === " ")
-//             if (audio.paused) {
-//                 audio.play();
-//                 jplay.src = "/assets/pause.svg";
-//             } else {
-//                 audio.pause();
-//                 jplay.src = "/assets/play.svg";
-//             }
-//     })
-// }
-// main()
 async function fetcher() {
-    let fetch1= await fetch("assets/Songs/");
-    let fetch2= await fetch1.text();
-    let fetch3=document.createElement("div");
-    fetch3.innerHTML=fetch2;
-    let fetch4= fetch3.getElementsByTagName("a");
-    let arr=[];
+    let fetch1 = await fetch("assets/Songs/");
+    let fetch2 = await fetch1.text();
+    let fetch3 = document.createElement("div");
+    fetch3.innerHTML = fetch2;
+    let fetch4 = fetch3.getElementsByTagName("a");
+    let arr = [];
     for (let index = 0; index < fetch4.length; index++) {
         if (fetch4[index].href.endsWith(".mp3")) {
             arr.push(fetch4[index].href);
         }
     }
     return arr;
-    
+
 }
 async function main() {
-    let songs1=await fetcher();
-    console.log(songs1); 
+    let songsarray = await fetcher();
+    let song = new Audio;
+    let playbtn = document.querySelector(".play");
+    let playlist = document.querySelector(".oip");
+    let eachsong = document.querySelector(".oip").getElementsByTagName("li");
+    let songname = document.querySelector(".songname");
+    let duration=document.querySelector(".dur");
 
-    
+    song.src = songsarray[0];
+ 
+
+
+
+
+    document.addEventListener("keydown", (e) => {
+        if (e.key == " ") {
+            if (song.paused) {
+                song.play();
+                playbtn.src = "assets/pause.svg"
+
+            }
+            else {
+                song.pause();
+                playbtn.src = "assets/play.svg"
+            }
+        }
+    })
+    playbtn.addEventListener("click", () => {
+        if (song.paused) {
+            song.play();
+            playbtn.src = "assets/pause.svg"
+        } else {
+            song.pause();
+            playbtn.src = "assets/play.svg"
+
+        }
+    })
+    for (let index = 0; index < songsarray.length; index++) {
+        let playlist1 = songsarray[index];
+        let playlist2 = playlist1.split("/Songs/")[1];
+        let playlist3 = decodeURIComponent(playlist2);
+        let playlist4 = playlist3.replaceAll(".mp3", "");
+        playlist.innerHTML += `<li class="normalhov music">${playlist4}</li>`;
+
+    }
+
+    for (let index = 0; index < songsarray.length; index++) {
+        eachsong[index].addEventListener("click", () => {
+            song.src = songsarray[index];
+            if (song.paused) {
+                song.play();
+                playbtn.src = "assets/pause.svg"
+            } else {
+                song.pause();
+                playbtn.src = "assets/play.svg"
+            }
+            songname1 = decodeURIComponent(songsarray[index]);
+            songname2 = songname1.split("/Songs/")[1];
+            songname3 = songname2.replaceAll(".mp3", "");
+            songname.innerHTML = songname3;
+            
+            
+        })
+    }
+    song.addEventListener("timeupdate",()=>{
+        let sec=Math.floor(song.currentTime%60);
+        let min=Math.floor(song.currentTime/60);
+        let osec=Math.floor(song.duration%60);
+        let omin=Math.floor(song.duration/60);
+        
+        duration.innerHTML=`${min}:${sec}/${omin}:${osec}`;
+    })
+
+
+
 }
 main();
